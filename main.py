b94e7d1dc57a557f357d3cd2886f49db0ddb0ee5
aalexandrov067-dev <a.alexandrov067@gmail.com>
2026-04-26 19:24:17 +0300

    commit2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pygame import *
+widht=700
+height=500
+window=display.set_mode((widht,height))
+display.set_caption('Шутер')
+background = transform.scale(image.load('футболл.jfif'),(widht,height))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+clock = time.Clock()
+FPS=60
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+game= True
+
+
+
+
+while game:
+    window.blit(background, (0,0))
+    for i in event.get():
+        if i.type == QUIT:
+            game = False
+    display.update()
